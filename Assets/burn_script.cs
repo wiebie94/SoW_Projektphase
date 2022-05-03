0903c8c466fd3a5a115dfc109f58b4c7bacdb735
wiebie94 <34031465+wiebie94@users.noreply.github.com>
2022-05-03 15:34:47 +0200

    alex destroyed the shader

--- Assets/burn_script.cs
+++ Assets/burn_script.cs
@@ -7,18 +7,25 @@ public class burn_script : MonoBehaviour
     // Start is called before the first frame update
 
     public float speed = 0.001f;
+
+    public float dissolveSpeed = 0.001f;
     public float dissolve_threshold = 0.75f;
     public Renderer renderer;
     private float burn_amount;
     private float burn_default;
     private float dissolve_amount;
     private float dissolve_default;
+
+    [SerializeField] Material burningMat;
+    private Material lastDefaultMat;
+    
+
     private bool go = false;
     void Start()
     {
         renderer = GetComponent<Renderer> ();
-        burn_default = renderer.material.GetFloat("prop_burn");
-        dissolve_default = renderer.material.GetFloat("prop_dissolve");
+        burn_default = burningMat.GetFloat("prop_burn");
+        dissolve_default = burningMat.GetFloat("prop_dissolve");
         burn_amount = burn_default;
         dissolve_amount = dissolve_default;
         
@@ -29,26 +36,56 @@ public class burn_script : MonoBehaviour
     {
 
         if(Input.GetKey("b")){
+
+            Material[] mats = {renderer.material, burningMat};
+
+            lastDefaultMat = renderer.material;
+
+            Debug.Log("in b");
+
+            renderer.materials = mats;
+
+            burn_amount = 0;
+
+            burningMat.SetFloat("prop_dissolve", 0);
+            dissolve_amount = 0;
+
             go = true;
         }
 
         if(Input.GetKey("r")){
             go = false;
             dissolve_amount = dissolve_default;
-            renderer.material.SetFloat("prop_dissolve", dissolve_default);
-            burn_amount = burn_default;
-            renderer.material.SetFloat("prop_burn", burn_default);
-        }
 
+             Material[] mats = {lastDefaultMat};
+            renderer.materials = mats;
+
+            burningMat.SetFloat("prop_dissolve", dissolve_default);
+            burn_amount = burn_default;
+            burningMat.SetFloat("prop_burn", burn_default);
+        }
+    
         if(go){
-            burn_amount += speed;
+            burn_amount += speed * Time.deltaTime;
+
+            Debug.Log("burnamount " + burn_amount + " dissolve " + dissolve_amount);
             if(burn_amount < dissolve_threshold){
-                renderer.material.SetFloat("prop_burn", burn_amount);
+                burningMat.SetFloat("prop_burn", burn_amount);
+
+                Debug.Log("in if");
             } 
             else{
-                dissolve_amount += speed;
-                renderer.material.SetFloat("prop_dissolve", dissolve_amount);
+                
+                Debug.Log("in else");
+
+
+                Material[] mats = {burningMat};
+                renderer.materials = mats;
+
+                dissolve_amount += dissolveSpeed * Time.deltaTime;
+                burningMat.SetFloat("prop_dissolve", dissolve_amount);
             }
         }
+        
     }
 }
