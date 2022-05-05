91f267f80498fb3de6f220b89211e162af6e8203
MihawkCola <32357495+MihawkCola@users.noreply.github.com>
2022-05-05 16:20:26 +0200

    fast fertig fehlt noch balacing

--- Assets/Scripts/TeleportKugelScript.cs
+++ Assets/Scripts/TeleportKugelScript.cs
@@ -9,69 +9,107 @@ public class TeleportKugelScript : MonoBehaviour
     [SerializeField] private bool destroy = true;
     [SerializeField] private bool tagTeleport = true;
 
+    [SerializeField] private float teleportDelay = 2.0f;
+    [SerializeField] private int colisionCounter = 0;
+    [SerializeField] private float destroyMaxTime = 5.0f;
+
     [SerializeField] private string playerGameObjectName = "XR Origin";
-    [SerializeField] private string leftHandGameObjectName = "LeftHand Controller";
-    [SerializeField] private string rightHandGameObjectName = "RightHand Controller";
+
+    private Rigidbody rb;
+    private MeshRenderer m;
 
     private ActionBasedController controller;
 
     private GameObject player;
 
+    private IKugelDestroy manager;
+
+    private bool isTeleportation = false;
+    private float destroyTime = 0;
+
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        this.manager = GameObject.Find("TeleportManager").GetComponent<IKugelDestroy>();
+        if (this.manager == null) Debug.Log("Achtung es befindet sich kein TeleporManager in der Scene");
+
+        this.rb = this.GetComponent<Rigidbody>();
+        this.m = this.GetComponent<MeshRenderer>();
+
         this.player =  GameObject.Find(playerGameObjectName);
-        if (player != null)
-        {
-            GameObject leftHandGameobject = GameObject.Find(leftHandGameObjectName);
-            GameObject rightHandGameobject = GameObject.Find(rightHandGameObjectName);
-
-            this.controller = rightHandGameobject.GetComponent<ActionBasedController>();   
-            this.controller.selectAction.action.canceled += enable;
-
-            this.controller = rightHandGameobject.GetComponent<ActionBasedController>();
-            this.controller.selectAction.action.canceled += enable;
-        }
-        else 
-        {
-            Debug.Log("Objektname vom Spieler ist Falsch oder nicht angegeben");
-        }
+        if (player == null) Debug.Log("Objektname vom Spieler ist falsch oder nicht angegeben");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //######### Destroy over Time #########
+        /*if (this.isEnable && !this.isTeleportation) this.destroyTime += Time.deltaTime;
+
+        if (this.destroyMaxTime < this.destroyTime) Object.Destroy(this.gameObject);*/
+        Debug.Log(this.rb.velocity.magnitude);
+
+        if (this.isEnable && this.rb.velocity.magnitude < 0.05) Object.Destroy(this.gameObject);
+
+    }
+
+    void OnDestroy()
+    {
+        if (this.manager != null) manager.isDestroyed();
     }
     void OnCollisionEnter(Collision collision)
     {
+        //######### Collision checks #########
         GameObject collisionGameObject = collision.gameObject;
         if (!isEnable) return;
 
         if (collisionGameObject.tag == "Hand" || collisionGameObject.tag == "Player") return;
 
+        if(this.isEnable) colisionCounter++;
+
         if (this.tagTeleport && collisionGameObject.tag != "Teleport") return;
 
-        this.teleportation(collision.contacts[0].point);
+
+        if (destroy) {
+            this.rb.isKinematic = true;
+            this.rb.detectCollisions = false;
+            this.m.enabled = false;
+        }
+
+
+        //######### Teleportaion #########
+        StartCoroutine(this.teleportation(collision.contacts[0].point));
+
         this.isEnable = false;
+
         Debug.Log("Teleport");
-        if (destroy) Object.Destroy(this.gameObject);
         
     }
 
-    private void teleportation(Vector3 point) 
+    private IEnumerator teleportation(Vector3 point) 
     {
-        // Funktioniert noch nicht aus Gründen
-        //yield return new WaitForSeconds(2f);
-        Debug.Log("test");
+        this.isTeleportation = true;
+        this.startingTelportAnimation();
+        yield return new WaitForSeconds(this.teleportDelay);
+  
         float playerColiderOffsetY = this.player.GetComponent<Collider>().bounds.extents.y;
 
-        this.player.transform.position = point + Vector3.up * playerColiderOffsetY;
+        this.player.transform.position = (point + Vector3.up) * playerColiderOffsetY;
+
+        if (destroy) {
+            Object.Destroy(this.gameObject);
+        } 
 
     }
 
-    private void enable(InputAction.CallbackContext obj)
+    private void startingTelportAnimation()
+    {
+        //ToDo partikel Animation ausführen
+    }
+
+    public void activate()
     {
         this.isEnable = true;
+        this.destroyTime = 0;
     }
 }
