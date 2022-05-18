de901ae520e33bb2b044973575f4276f718f1373
AGreten <greten.alfons@gmail.com>
2022-05-18 14:47:44 +0200

    Added Teleport and Key Highlight

---
+++ Assets/Rotate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed, 0) * Time.deltaTime);
+    }
+}
