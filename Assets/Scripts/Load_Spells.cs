3b1a494a4ef74c5fa37c4d91f6f3b4f4aca8fc40
AGreten <greten.alfons@gmail.com>
2022-05-23 18:06:56 +0200

    Loading circle added

---
+++ Assets/Scripts/Load_Spells.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_Spells : MonoBehaviour
+{
+    public GameObject rim;
+    public GameObject center;
+    RectTransform centerImage;
+
+    public float maxSize;
+    public float time;
+    float timePassed = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rim.SetActive(false);
+        center.SetActive(false);
+        centerImage = center.GetComponent<RectTransform>();
+
+        rim.GetComponent<RectTransform>().sizeDelta = new Vector2(maxSize, maxSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime; 
+        if(timePassed <= time){
+            centerImage.sizeDelta = new Vector2(((timePassed) / time) * maxSize, ((timePassed) / time) * maxSize);
+        } else {
+            rim.SetActive(false);
+            center.SetActive(false);
+        }
+    }
+
+    public void loadMenu()
+    {
+        rim.SetActive(true);
+        center.SetActive(true);
+        centerImage.sizeDelta = new Vector2(0,0);
+        timePassed = 0;
+    }
+}
