440597e08aea738702e98394b92c51c010560479
Sasha <79443329+Drekys7@users.noreply.github.com>
2022-05-26 13:10:36 +0200

    Adding Gate

--- Assets/Scripts/ObjectsLogic/LeverLogic.cs
+++ Assets/Scripts/ObjectsLogic/LeverLogic.cs
@@ -1,23 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LeverLogic : MonoBehaviour
 {
     private HingeJoint hingeJoint;
 
-    private const float timeToCheckAfterCollision = 2;
-    private const float checkUpdateTime = 0.05f;
-
-    private float time;
-
-    private Coroutine leverCheckCoroutine;
+    private const float checkUpdateTime = 0.2f;
 
     private float minLimit;
     private float maxLimit;
 
     private float springDamper;
     private float springForce;
+
+    [SerializeField] bool upOnStart;
+
+    [SerializeField] UnityEvent eventUp;
+    [SerializeField] UnityEvent eventDown;
 
     void Start()
     {
@@ -29,33 +30,36 @@ public class LeverLogic : MonoBehaviour
         springDamper = hingeJoint.spring.damper;
         springForce = hingeJoint.spring.spring;
 
-        Debug.Log(hingeJoint.limits.max);
+        if (upOnStart)
+            hingeJoint.spring = CreateSpring(maxLimit);
+        else
+            hingeJoint.spring = CreateSpring(minLimit);
+
+        StartCoroutine(LeverStateCheck());
     }
 
     IEnumerator LeverStateCheck()
     {
-        time = 0;
-        while (time < timeToCheckAfterCollision)
+        while (true)
         {
             Debug.Log("Lever update");
             if (hingeJoint.angle > 0 && hingeJoint.spring.targetPosition != maxLimit)
             {
                 hingeJoint.spring = CreateSpring(maxLimit);
+                eventUp.Invoke();
                 Debug.Log("CHange to max");
             }
                 
             else if (hingeJoint.angle < 0 && hingeJoint.spring.targetPosition != minLimit)
             {
                 hingeJoint.spring = CreateSpring(minLimit);
+                eventDown.Invoke();
                 Debug.Log("CHange to min");
             }
                 
 
             yield return new WaitForSeconds(checkUpdateTime);
-            time += checkUpdateTime;
         }
-
-        leverCheckCoroutine = null;
     }
 
     private JointSpring CreateSpring(float targetPos)
@@ -66,17 +70,5 @@ public class LeverLogic : MonoBehaviour
         spring.targetPosition = targetPos;
 
         return spring;
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (leverCheckCoroutine == null)
-        {
-            leverCheckCoroutine = StartCoroutine(LeverStateCheck());
-        }
-        else
-        {
-            time = 0;
-        }
     }
 }
