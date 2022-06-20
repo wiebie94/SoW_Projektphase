5837bbb99d36fca23f945ed82debb90bb8e4a262
AGreten <greten.alfons@gmail.com>
2022-06-20 11:42:54 +0200

    GateController edited / Implemented GateController in Halle 2.0

--- Assets/Scripts/ObjectsLogic/GateController.cs
+++ Assets/Scripts/ObjectsLogic/GateController.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class GateController : MonoBehaviour
 {
-    public GameObject[] gates;
-    public GameObject[] levers;
-    public GameObject[] levels;
-    public GameObject[] levelPostitions;
-    private GameObject g1;
+    public GameObject[] gates;                  // Tore die bewegt werden
+    public GameObject[] levers;                 // Hebel der jeweiligen Tore
+    public GameObject[] levels;                 // Level Prefabs
+    public GameObject[] levelPostitions;        // Positionen an denen die Level instantiiert werden
+    private GameObject activeGate;                      
 
-    [SerializeField] float gateCloseTime = 2.0f;
+    [SerializeField] float gateCloseTime = 1.75f;
 
+    // Oeffnet ein Tor zum dazugehoerigen Hebel und schliesst alle anderen Tore
     public void Open(GameObject lever) {
         for(int i = 0; i < gates.Length; i++) {
             if(lever == levers[i]) {
                 gates[i].GetComponent<GateLogic>().OpenGate();
-                //levels[i].SetActive(true);
-                Destroy(g1);
-                g1 = Instantiate(levels[i], levelPostitions[i].transform, false);
+                // Zerstoert das vorherig instantiierte Level
+                Destroy(activeGate);
+                // Instantiieren des Levels
+                activeGate = Instantiate(levels[i], levelPostitions[i].transform, false);
             }
             else {
+                // Schliessen der anderen Tore
                 gates[i].GetComponent<GateLogic>().CloseGate();
-                //StartCoroutine(HideLevel(levels[i]));
             }
         }
     }
@@ -31,16 +33,19 @@ public class GateController : MonoBehaviour
         for(int i = 0; i < gates.Length; i++) {
             if(lever == levers[i]) {
                 gates[i].GetComponent<GateLogic>().CloseGate();
-                StartCoroutine(HideLevel(levels[i]));
+                // Checkt ob ein Level bereits in einem Positions Objekt instantiiert wurde
+                if(levelPostitions[i].transform.childCount > 0){
+                    StartCoroutine(HideLevel());
+                }                
             }
         }
     }
 
-    private IEnumerator HideLevel(GameObject level)
+    private IEnumerator HideLevel()
     {
+        // Level wird nach bestimmter Zeit zerstoert, damit das Tor vorher noch schliessen kann
         yield return new WaitForSeconds(gateCloseTime);
 
-        //level.SetActive(false);
-        Destroy(g1);
+        Destroy(activeGate);
     }
 }
