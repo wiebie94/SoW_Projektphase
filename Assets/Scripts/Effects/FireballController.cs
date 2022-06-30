8a73f621fb17b5d9722e2e86147a8f1c5f34cdbf
wiebie94 <34031465+wiebie94@users.noreply.github.com>
2022-06-30 03:41:46 +0200

    kindle fertig für alle objecte

--- Assets/Scripts/Effects/FireballController.cs
+++ Assets/Scripts/Effects/FireballController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,12 +11,16 @@ public class FireballController : MonoBehaviour
     public GameObject steam_prefab;
     public GameObject flame_prefab;
     public GameObject kindle_flame_prefab;
+    public GameObject kindle_flame_prefab_big;
     public GameObject dissolveSound_prefab;
+    public GameObject candleLight_prefab;
     private GameObject explosion;
     private GameObject steam;
     private GameObject kindleFlame;
+    private GameObject kindleFlameBig;
     private GameObject woodBurn;
     private GameObject dissolveBurnSound;
+    private GameObject candleFlame;
     public float steamDuration;
     public float explosionDuration;
     public float burnDuration;
@@ -36,10 +41,7 @@ public class FireballController : MonoBehaviour
     public void OnCollisionEnter(Collision other) {
 
         if (this.firstHit || other.gameObject.tag == "Reflect") return;
-        this.firstHit = true;
-
-        explosion = Instantiate(explosion_prefab, other.GetContact(0).point, Quaternion.identity);
-        Destroy(explosion, explosionDuration);        
+        this.firstHit = true;             
 
         //burn something
         if(other.gameObject.tag == "Burnable"){
@@ -50,15 +52,29 @@ public class FireballController : MonoBehaviour
         }
 
         //kindle something
-        if(other.gameObject.tag == "Kindle") {
-            spawnPos = other.gameObject.GetComponent<Transform>().position + other.gameObject.GetComponent<KindleOffset>().getOffset();
-            kindleFlame = Instantiate(kindle_flame_prefab, spawnPos, Quaternion.identity);
-            //other.gameObject.GetComponent<CandleValue>().offset
+        if(other.gameObject.tag == "Kindle" || other.gameObject.tag == "KindleBig") {
+            
+            Vector3 offset = other.transform.GetComponent<KindleOffset>().getOffset();
+            Vector3 offsetforward = new Vector3(0,0,0);
+            offsetforward.z = offset.z;
+            spawnPos = other.gameObject.GetComponent<Transform>().position;
+            spawnPos.y += offset.y;
+            if(other.gameObject.tag == "kindle"){
+                kindleFlame = Instantiate(kindle_flame_prefab, spawnPos, Quaternion.identity);
+            }
+            else{
+                kindleFlame = Instantiate(kindle_flame_prefab_big, spawnPos, Quaternion.identity);
+            }
+            kindleFlame.transform.Rotate(Vector3.up, other.gameObject.GetComponent<Transform>().rotation.eulerAngles.y);
+            kindleFlame.transform.Translate(offsetforward, Space.Self);
+            other.transform.GetChild(other.transform.GetChildCount()-1).gameObject.active = true;
+            candleFlame = Instantiate(candleLight_prefab, kindleFlame.transform.position, Quaternion.identity);
+
             Debug.Log("kindle stuff");
         }
 
         //burn on wood other.gameObject.tag == "WoodBurn"
-        if (true) {
+        if (other.gameObject.tag == "WoodBurn") {
             woodBurn = Instantiate(flame_prefab, other.GetContact(0).point, Quaternion.identity);
             Destroy(woodBurn, burnDuration);
         }
@@ -93,5 +109,11 @@ public class FireballController : MonoBehaviour
             if (rb != null)
                 rb.AddExplosionForce(this.explosionPower, position, this.explosionRadius, 3.0F);
         }
+    }
+
+    private void rotateFlame(GameObject flame, Quaternion degree, Vector3 offsetForward){
+        flame.transform.Rotate(Vector3.up, degree.eulerAngles.y);
+        flame.transform.Translate(offsetForward, Space.Self);
+       
     }
 }
