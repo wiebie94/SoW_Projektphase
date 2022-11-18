8ff28ef4434d77bd06f095cfa3e59d693aac2b5e
wiebie94 <34031465+wiebie94@users.noreply.github.com>
2022-11-18 16:58:38 +0100

    neues level start

--- Assets/Scripts/ObjectsLogic/GateController.cs
+++ Assets/Scripts/ObjectsLogic/GateController.cs
@@ -8,9 +8,12 @@ public class GateController : MonoBehaviour
     public GameObject[] levers;                 // Hebel der jeweiligen Tore
     public GameObject[] levels;                 // Level Prefabs
     public GameObject[] levelPostitions;        // Positionen an denen die Level instantiiert werden
-    private GameObject activeGate;                      
+    private GameObject activeGate;      
+
+    private Rigidbody rbOfLever;               
 
     [SerializeField] float gateCloseTime = 1.75f;
+    [SerializeField] float leverStopTime = 2.5f;
 
     // Oeffnet ein Tor zum dazugehoerigen Hebel und schliesst alle anderen Tore
     public void Open(GameObject lever) {
@@ -21,6 +24,8 @@ public class GateController : MonoBehaviour
                 Destroy(activeGate);
                 // Instantiieren des Levels
                 activeGate = Instantiate(levels[i], levelPostitions[i].transform, false);
+                rbOfLever = lever.GetComponent<Rigidbody>();
+                //StartCoroutine(StopLever(rbOfLever));
             }
             else {
                 // Schliessen der anderen 
@@ -34,6 +39,8 @@ public class GateController : MonoBehaviour
         for(int i = 0; i < gates.Length; i++) {
             if(lever == levers[i]) {
                 gates[i].GetComponent<GateLogic>().CloseGate();
+                rbOfLever = lever.GetComponent<Rigidbody>();
+                //StartCoroutine(StopLever(rbOfLever));
                 // Checkt ob ein Level bereits in einem Positions Objekt instantiiert wurde
                 if(levelPostitions[i].transform.childCount > 0){
                     StartCoroutine(HideLevel());
@@ -48,5 +55,14 @@ public class GateController : MonoBehaviour
         yield return new WaitForSeconds(gateCloseTime);
 
         Destroy(activeGate);
+    }
+
+    //stops the lever for 2.5 seconds to prevent 
+    private IEnumerator StopLever(Rigidbody rb){
+        
+        rb.isKinematic = true;
+        yield return new WaitForSeconds(leverStopTime);
+        rb.isKinematic = false;
+        
     }
 }
